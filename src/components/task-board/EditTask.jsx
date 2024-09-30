5199d71e8cb07f208986bac5fcbb50e09f66cc20
pistaacademy <pista.academy@gmail.com>
2024-09-30 05:43:23 +0330

    Session-13

--- src/components/task-board/EditTask.jsx
+++ src/components/task-board/EditTask.jsx
@@ -3,18 +3,51 @@ import TextInput from "../utils/text-input/TextInput";
 import TextArea from '../utils/text-area/TextArea';
 import { Button } from '@mui/material';
 import { useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const statusOptions = [
+    { label: "Open", value: "Open"},
+    { label: "In Progress", value: "InProgress"},
+    { label: "In Verification", value: "InVerification"},
+    { label: "Done", value: "Done"},
+]
 
 const EditTask = ({showEditTask, closeDrawer}) => {
 
-    const [status, setStatus] = useState("")
+    const [taskTitle, setTaskTitle] = useState("");
+    const [taskDesc, setTaskDesc] = useState("");
+    const [status, setStatus] = useState("");
+
+    const statusHandler = (e) => {
+        setStatus(e.target.value)
+    }
+
+    const handleTextInput = (e) => {
+        setTaskTitle(e.target.value)
+    }
+
+    const handleTextArea = (e) => {
+        setTaskDesc(e.target.value)
+    }
+
+    const updateBtnHandler = () => {
+        console.log("title", taskTitle)
+        console.log("desc", taskDesc)
+        console.log("status", status)
+    }
+
 
     return (
         <Drawer open={showEditTask} onClose={() => closeDrawer()} PaperProps={{ sx: {width: 400, padding: "1rem" }}}>
             <h3>Edit Task</h3>
-            <TextInput label="Task Title" />
-            <TextArea label="Task Desc" />
+            <TextInput handleTextInput={handleTextInput} label="Task Title" />
+            <TextArea handleTextArea={handleTextArea} label="Task Desc" />
 
-            <Box sx={{ minWidth: 120 }}>
+            <Box sx={{ minWidth: 120, width: "90%", marginBottom: "1rem" }}>
                 <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Status</InputLabel>
                 <Select
@@ -22,18 +55,20 @@ const EditTask = ({showEditTask, closeDrawer}) => {
                     id="demo-simple-select"
                     value={status}
                     label="Status"
-                    onChange={statusHandler}
+                    onChange={(e) => statusHandler(e)}
                 >
-                <MenuItem value={10}>Open</MenuItem>
-                <MenuItem value={20}>Done</MenuItem>
-                <MenuItem value={30}>Pending</MenuItem>
+
+                {statusOptions.map((option)=> {
+                    return <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+                })}
+                
                 </Select>
                 </FormControl>
             </Box>
 
 
             <div>
-                <Button sx={{ marginRight: ".5rem"}} variant="contained">Update</Button>
+                <Button sx={{ marginRight: ".5rem"}} variant="contained" onClick={updateBtnHandler}>Update</Button>
                 <Button  onClick={() => closeDrawer()} variant='outlined'>Cancel</Button>
             </div>
         </Drawer>
